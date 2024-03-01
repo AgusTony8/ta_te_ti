7be53c774ce58e253cb47fb7cbd378c81b535074
Agustin Sch <84196634+AgusTony8@users.noreply.github.com>
2024-03-01 02:49:16 -0300

    add wining layout and a reset button

--- app/game.js
+++ app/game.js
@@ -1,11 +1,27 @@
 // board Factory as a IIFE----------------------------------------------------------
-const gameBoard = (function() {
+function newGameBoard() {
     //getting all the tiles on the board 
     const square = document.querySelectorAll(".casilla")
     
+    function clearBoard(){
+        square.forEach((cuadrados,i) => {
+            cuadrados.textContent = ''
+            cuadrados.classList.remove("win")
+        })
+        document.querySelector(".winning-module").style.display = "none"
+    }
+    //restart game
+    document.getElementById('restart').addEventListener('click', () =>{
+        clearBoard();
+    })
+    
+
+    //turn counter
+    let turn = 0
+    const nextTurn = () => turn++
+    const getTurn = () => turn;
     function someoneWon(){
         const squareValueArr = Array.from(square).map(sq => sq.textContent)
-        console.log('squareValueArr:', squareValueArr)
         
         // row control
         for (let i = 0; i < 9; i+=3) {
@@ -30,8 +46,8 @@ const gameBoard = (function() {
             (squareValueArr[0] === squareValueArr[4]) && 
             (squareValueArr[0] === squareValueArr[8])) return [0,4,8];
         if(squareValueArr[2] && 
-            (squareValueArr[2] === tablero[4]) && 
-            (squareValueArr[2] === tablero[6])) return [2,4,6];
+            (squareValueArr[2] === squareValueArr[4]) && 
+            (squareValueArr[2] === squareValueArr[6])) return [2,4,6];
 
         //tie control
         if(squareValueArr.includes("")) return false;
@@ -40,29 +56,59 @@ const gameBoard = (function() {
     const mark = (player) =>{
         square.forEach((cuadrados,i) =>{
             cuadrados.addEventListener("click", () =>{
-                cuadrados.innerText = player.simbol;
-                console.log('someoneWon:', someoneWon());
 
+                //if a tie or someone won, the divs dosent respond any more
+                if (someoneWon() !== false) {return}
+                cuadrados.innerText = player.simbol;
             })
         })
     }
 
 
-    return{mark}
+    return{mark, getTurn, nextTurn, square, someoneWon}
 
-})();
+};
 
-gameBoard.mark(a)
 
 
 //game state factory----------------------------------------------------------
 
-function createGame(gb){
-    const p1 = {simbol: "X"}
-    const p2 = {simbol: "O"}
-    const turn = 0
+const game = (function (){
+
+    const p1 = {simbol: "X"} //starts at turn 1 for some reason
+    const p2 = {simbol: "O"} //starts at 2
+    const gameBoard =  newGameBoard();
+
+    function announceWinner(positions, turn){
+        console.log('positions,turn:', positions,turn)
+        positions.forEach(ps => gameBoard.square[ps].classList.toggle("win", true))
+        document.querySelector(".winning-module").style.display = "block"
+    }
+
+
     function makeMove(){
+        console.log('gameBoard.getTurn():', gameBoard.getTurn())
+        if ( (gameBoard.getTurn()%2) == 0) {
+            gameBoard.mark(p1);
+            
+        }
+        else{
+             gameBoard.mark(p2)
+        };
+        //if there was a winner in this turn, we announce it
+        if (gameBoard.someoneWon() !== false){
+            announceWinner(gameBoard.someoneWon(), gameBoard.getTurn())
+        };
+        gameBoard.nextTurn();
         
     }
 
-}
+    //turn progretion
+    gameBoard.square.forEach((cuadrados,i) =>{
+        cuadrados.addEventListener("click", () =>{
+            makeMove();
+        })
+    })
+
+})();
+
